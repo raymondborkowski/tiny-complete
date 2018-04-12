8866fe9983c73d236cec57ada79879eb28036a02
Ray Borkowski <rborkows@umich.edu>
2018-04-12 17:08:53 -0400

    RB: Add fake test for jasmine

--- spec/unit/tiny-complete.spec.js
+++ spec/unit/tiny-complete.spec.js
@@ -2,25 +2,6 @@ describe('TinyComplete', function () {
     // var Player = require('../../index.js');
 
     it('this is an example that should always pass', function () {
-        if (true === false) {
-            expect(true).toBe(true);
-            expect(9).toBe(true);
-        }
-        if (true === false) {
-            expect(9).toBe(true);
-        }
-        if (true === false) {
-            expect(9).toBe(true);
-        }
-        if (true === false) {
-            expect(9).toBe(true);
-        }
-        if (true === false) {
-            expect(9).toBe(true);
-        }
-        if (true === false) {
-            expect(9).toBe(true);
-        }
+        expect(true).toBe(true);
     });
 });
-jasmine;
