b35a6ed5f3f0760eda9c91035b62800db16d032e
Ray Borkowski <rborkows@umich.edu>
2018-04-12 16:49:29 -0400

    RB: Fix up readme

--- spec/unit/tiny-complete.spec.js
+++ spec/unit/tiny-complete.spec.js
@@ -1,8 +1,26 @@
 describe('TinyComplete', function () {
     // var Player = require('../../index.js');
 
-    it('this is an example that should alawys pass', function () {
-        expect(true).toBe(true);
+    it('this is an example that should always pass', function () {
+        if (true === false) {
+            expect(true).toBe(true);
+            expect(9).toBe(true);
+        }
+        if (true === false) {
+            expect(9).toBe(true);
+        }
+        if (true === false) {
+            expect(9).toBe(true);
+        }
+        if (true === false) {
+            expect(9).toBe(true);
+        }
+        if (true === false) {
+            expect(9).toBe(true);
+        }
+        if (true === false) {
+            expect(9).toBe(true);
+        }
     });
 });
 jasmine;
