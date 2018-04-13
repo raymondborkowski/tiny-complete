e5036e9fcbfc7e981a62ce19963c22776a1f8e36
Raymond Borkowski <raymondborkowski@raymonds-mbp.home>
2018-04-13 00:14:56 -0400

    RB: Start creating tiny-complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function TinyComplete() {
-    // var arrOfVals = ['detroit', 'nyc', 'grand rapids'];
-    console.log('hello');
+function request(url, cb) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {cb(xmlHttp.responseText);}
+    };
+    xmlHttp.open('GET', url, true);
+    xmlHttp.send(null);
 }
 
-TinyComplete.prototype.nuke = function () {
-    console.log('nuked');
-};
+function createUrlEncodedString(val) {
+    return encodeURIComponent(val);
+}
+
+function bindToInput(el, cb) {
+    el.addEventListener('input', function (e) {
+        cb(createUrlEncodedString(e.srcElement.value));
+    });
+}
+
+function TinyComplete(_options) {
+    var options = _options || {};
+    var inputEl = document.getElementById(options.id);
+
+    options.setArrVals(request, createUrlEncodedString(inputEl.value)); // Request for initial value
+    bindToInput(inputEl, function (query) {
+        if (typeof options.setArrVals === 'function') {
+            options.setArrVals(request, query);
+        }
+    });
+}
+
+TinyComplete.prototype.nuke = function () {};
+
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = TinyComplete;
